d4396aab784b062e770ee134d1d0648cd7ebd079
Kentama7 <kentama.7m@gmail.com>
2020-01-19 19:04:06 +0900

    add react-admin

---
+++ front-end/src/App.js
@@ -0,0 +1,10 @@
+// in src/App.js
+import React from 'react';
+import { Admin } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => <Admin dataProvider={dataProvider} />;
+
+export default App;
+

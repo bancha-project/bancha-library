b57352b3d2fe1ea4801dd4627760c02c7e8c0bd8
Kentama7 <kentama.7m@gmail.com>
2020-02-01 21:01:41 +0900

    add paging

--- src/main/kotlin/com/github/banchaproject/banchalibrary/domain/repository/book/BookRepository.kt
+++ src/main/kotlin/com/github/banchaproject/banchalibrary/domain/repository/book/BookRepository.kt
@@ -3,10 +3,10 @@ package com.github.banchaproject.banchalibrary.domain.repository.book
 import com.github.banchaproject.banchalibrary.domain.model.Book
 import io.micronaut.data.annotation.Id
 import io.micronaut.data.jdbc.annotation.JdbcRepository
-import io.micronaut.data.repository.CrudRepository
+import io.micronaut.data.repository.PageableRepository
 
 @JdbcRepository
-interface BookRepository : CrudRepository<Book, Long> {
+interface BookRepository : PageableRepository<Book, Long> {
     // entityをそのまま渡せないっぽい。
     fun update(@Id id: Long?, title: String, author: String, summary: String, isbn: String)
 }
